6fb7d213b4f07310c883835484f4b46b4cc64ca6
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-08-10 21:11:43 +0530

    chore: contract deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [process.env.STAGING_PRIVATE_KEY!],
+      // gas: 2100000,
+      // gasPrice: 8000000000,
+    },
+  },
 };
 
 export default config;
